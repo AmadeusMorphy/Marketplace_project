22f7ce1a2e966b962dec147dac62c0750628394f
Nizar Masadeh <n.masadeh@futeric.com>
2024-12-03 15:59:32 +0300

    nothing

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    server: 'https://api.mrnzd.com/api/',
-    serverAuth: 'https://api.mrnzd.com/api/auth/'
-}
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    server: 'http://localhost:3000/api/',
-    serverAuth: 'http://localhost:3000/api/auth/'
-}
