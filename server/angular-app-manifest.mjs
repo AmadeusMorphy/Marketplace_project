5980f8ea9d01313430a83da814adb8c938c20d06
Nizar Masadeh <n.masadeh@futeric.com>
2024-12-18 09:54:28 +0300

    Updates

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -4,8 +4,8 @@ export default {
   inlineCriticalCss: true,
   routes: undefined,
   assets: new Map([
-['index.csr.html', {size: 6010, hash: '954c5108f1cdf249bea56715c32277483cc69311431e96ecbd7a2899d2a78279', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
-['index.server.html', {size: 1644, hash: 'bc43e9ed0c4fc05737a05cc75153b3bc35ee18ab6256c25e9958cbcafae7cf10', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
+['index.csr.html', {size: 6010, hash: 'f887e84980e4a66249dda932f153dd82d7039f60ed3999866abed0912e51a4df', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
+['index.server.html', {size: 1644, hash: '5e5f5accc96ea5f7c0d23ceb00f380b22bad73e1da6e4d9c2a41908161537310', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
 ['styles-7TEHMAKX.css', {size: 360214, hash: 'CSclMuDAHnQ', text: () => import('./assets-chunks/styles-7TEHMAKX_css.mjs').then(m => m.default)}], 
 ['theme-dark.css', {size: 198368, hash: 'kVaex0Lm6aI', text: () => import('./assets-chunks/theme-dark_css.mjs').then(m => m.default)}], 
 ['theme-light.css', {size: 198060, hash: 'uiI7yUNHyNw', text: () => import('./assets-chunks/theme-light_css.mjs').then(m => m.default)}]
