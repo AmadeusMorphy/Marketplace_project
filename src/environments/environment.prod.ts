205bee965617ad7d45b973e6b268ff45d465d84a
Nizar Masadeh <n.masadeh@futeric.com>
2024-12-18 09:45:00 +0300

    no need to ignore env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+        server: 'https://api.nzrm.dev/api/',
+        serverAuth: 'https://api.nzrm.dev/api/auth/',
+        IpUrl: 'https://api.ipify.org?format=json',
+        locationByIp: 'http://ip-api.com/json/',
+        detailedLocation: 'http://ipwho.is/',
+        userAgent: 'https://api.ipgeolocation.io/user-agent?apiKey=8fa47f7f36f5437aa9d69bf5776e3cbe'
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    server: 'http://localhost:3000/api/',
+    serverAuth: 'http://localhost:3000/api/auth/',
+    IpUrl: 'https://api.ipify.org?format=json',
+    locationByIp: 'http://ip-api.com/json/', //insert Ip after the slash
+    detailedLocation: 'http://ipwho.is/', //insert Ip after the slash
+    userAgent: 'https://api.ipgeolocation.io/user-agent?apiKey=8fa47f7f36f5437aa9d69bf5776e3cbe'
+}
